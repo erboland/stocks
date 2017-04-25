c4a0360541265f401bdf2c07c0a896fb837a46bb
skull8888888 <skull8888888@gmail.com>
2017-04-25 23:16:00 +0600

    some sugar

--- stocksApp/CompanyTableViewController.swift
+++ stocksApp/CompanyTableViewController.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 
-class CompanyTableViewController: UITableViewController {
+class CompanyTableViewController: UITableViewController, TimeRangeControlDelegate {
 
     let sections = [" News", " Statistics", " Orders"]
     var cell: UITableViewCell!
     
+    var lineGraphView: LineGraphView!
+    var timeRangeControl: TimeRangeControl!
+    var stockPriceView: StockPriceView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        stockPriceView = StockPriceView(frame: CGRect(x: 0, y: 8, width: view.frame.width, height: 100))
+        
+        lineGraphView = LineGraphView(frame: CGRect(x: 16, y: 108, width: view.frame.width - 32, height: 100))
+        lineGraphView.lineWidth = 3
+        lineGraphView.lineColor = UIColor.white
+        lineGraphView.backgroundColor = UIColor.clear
+        
+        timeRangeControl = TimeRangeControl(frame: CGRect(x: 0, y: 216, width: view.frame.width, height: 40))
+        timeRangeControl.delegate = self
+        
+        let wrapperView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 256))
+        
+        wrapperView.backgroundColor = UIColor(hex: StockEnum.mainColor.rawValue)
+        wrapperView.addSubview(stockPriceView)
+        wrapperView.addSubview(lineGraphView)
+        wrapperView.addSubview(timeRangeControl)
+        
+        
+        Socket().onStocksOf("GOOGL") { data in
+            print(data)
+        }
+        
+        lineGraphView.showStocksFor(.today, symbol: "GOOGL")
+        
+        tableView.tableHeaderView = wrapperView
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-
+    }
+    
+    func selectedChanged(range: TimeRangeEnum) {
+        self.lineGraphView.showStocksFor(range, symbol: "GOOGL")
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,8 +60,7 @@ class CompanyTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        
+       
         return 3
     }
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -71,7 +98,7 @@ class CompanyTableViewController: UITableViewController {
             case 0:
                 cell.firstLabel.text = "OPEN"
                 cell.secondLabel.text = " VOLUME"
-                print("ok")
+              
             case 1:
                 cell.firstLabel.text = "HIGH"
                 cell.secondLabel.text = " AVG VOl"
@@ -103,11 +130,11 @@ class CompanyTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch indexPath.section {
         case 0:
-            return 100
+            return 70
         case 1:
             return 50
         case 2:
-            return 100
+            return 70
         default:
             return 100
         }
@@ -115,15 +142,14 @@ class CompanyTableViewController: UITableViewController {
  
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let title = self.sections[section]
-        let view = UIView()
-
+        
         let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 50))
         titleLabel.text = title
         titleLabel.textAlignment = .left
-        titleLabel.font = UIFont(name: "AvenirNext-Bold", size: 20)
+        titleLabel.font = UIFont(name: "AvenirNext-Medium", size: 20)
+        titleLabel.backgroundColor = UIColor.white
         
-        
-        return view
+        return titleLabel
     }
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
